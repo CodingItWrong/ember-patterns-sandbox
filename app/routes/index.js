3248bdf43898535a964e41a1b16861145f75930c
Josh Justice <josh.justice@accesscfa.com>
2020-06-23 06:50:30 -0400

    Add reloading

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,13 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
 export default class IndexRoute extends Route {
   model() {
     return this.store.findAll('widget');
+  }
+
+  @action
+  refreshModel() {
+    this.refresh();
   }
 }
